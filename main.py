ecfe0ea46fb0a3aaba02ddabdb99c894ec3ad61d
rookie-leo <proleonardopa@gmail.com>
2020-05-13 22:43:54 -0300

    Nesse momento consegui salvar a data no qual o usuário ativa a primeira opção. Proximo paço, colher os dados de hora que deitou para dormir e  hora que acordou.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from datetime import date, time, datetime, timedelta
 
 def dia_atual():
+    #salva a data atual no arquivo para o usuário não tentar burlar.
     data_hj = datetime.now()
-    data_str = str(data_hj.strftime('%d/%m/%Y'))
-    #print(data_str)
+    data_str = data_hj.strftime('%d/%m/%Y')
+    str_data = str(data_str)
+    arquivo = open('Arquivo', 'w')
+    data_arq = str_data
+    arquivo.write(data_arq)
+    #print(data_arq)
+    arquivo.close()
 
 
 print('''
@@ -16,13 +22,19 @@ while True:
     [1] Adicionar horario.
     [2] Verificar informações cadastradas.
     [3] Sair'''))
+
     if opcao == 1:
-        data_atual = dia_atual()
-        arquivo = open('Arquivo', 'w')
-        arquivo.write(data_atual)
-        arquivo.close()
+        dia_atual()
+        dormir = str(input('Que horas você se deitou para dormir ontem: '))
+        acordar = str(input('Que horas você acordou hoje: '))
+        d1 = datetime.strptime(dormir, '%H:%M')
+        d2 = datetime.strptime(acordar, '%H:%M')
+
 
     elif opcao == 2:
         solicitacao = open('Arquivo', 'r')
         #str(solicitacao)
         print(solicitacao)
+
+    elif opcao == 3:
+        break
